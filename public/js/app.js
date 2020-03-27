98976972b3f0f8e9daa954ea5cd95d742c4db86b
Golap Hossain <hossain6857@gmail.com>
2020-03-27 08:04:22 +0600

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,4 @@
-// console.log("client side javasript file is loaded")
-// fetch('http://localhost:3000/weather?address=Boston').then((response)=>{
-//     response.json().then((data)=>{
-//         if(data.error){
-//             console.log(data.error)
-//         }
-//         else{
-//             console.log(data.forecast)
-//             console.log(data.location)
 
-//         }
-//     })
-// })
 const weatherForm=document.querySelector('form')
 const search=document.querySelector('input')
 const messageOne=document.querySelector('#message-1')
@@ -21,7 +9,7 @@ weatherForm.addEventListener('submit',(e)=>{
     const location=search.value
     messageOne.textContent='Loading...'
     messagetwo.textContent=''
-    fetch('http://localhost:3000/weather?address='+encodeURIComponent(location)).then((response)=>{
+    fetch('/weather?address='+encodeURIComponent(location)).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             messageOne.textContent=data.error
